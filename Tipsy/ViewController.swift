05a1fcd0965cf4b6275d03cab0e9240fbdcd668a
Jonathan Singer <jonathan.s.singer@gmail.com>
2020-05-25 22:54:36 -0500

    Initial Commit

---
+++ Tipsy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Jonathan Singer on 5/25/20.
+//  Copyright © 2020 Jonathan Singer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
