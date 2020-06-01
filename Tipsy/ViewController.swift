2117b2ab576ae4fa865b63024a319e744a3799d0
Jonathan Singer <jonathan.s.singer@gmail.com>
2020-05-31 23:11:49 -0500

    Add calculate tip method for billAmount change

--- Tipsy/ViewController.swift
+++ Tipsy/ViewController.swift
@@ -10,11 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billAmount: UITextField!
+    @IBOutlet weak var percentage: UILabel!
+    @IBOutlet weak var tipAmount: UILabel!
+    @IBOutlet weak var total: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func billAmountChange(_ sender: Any) {
+        calculateTip()
+    }
+    
+    @IBAction func sliderChange(_ sender: Any) {
+    }
+    
+    func calculateTip(){
+        let bill = Double(billAmount.text!)!
+        let perc = Double(percentage.text!)!
+        let tip = bill*(perc/100)
+        let totalBill = tip+bill
+        tipAmount.text = String(tip)
+        total.text = String(totalBill)
+    }
 }
 
